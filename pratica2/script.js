f353dcdb7b9230d02e514c2009052e1cf432fd02
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2024-02-02 11:58:33 -0300

    Exercícios DOM-I

---
+++ pratica2/script.js
@@ -0,0 +1,18 @@
+const nome = document.getElementById('nome').value;
+console.log(nome);
+
+const endereco = document.getElementById('endereco').value;
+console.log(endereco);
+
+const email = document.getElementById('email').value;
+console.log(email);
+email.value = " ";
+
+const limpaform = () => {
+  document.getElementById('nome').value = "";
+  document.getElementById('endereco').value = "";
+  document.getElementById('email').value = "";
+  document.getElementById('nome-in').innerHTML = nome;
+  document.getElementById('endereco-in').innerHTML = endereco;
+  document.getElementById('email-in').innerHTML = email;
+}
